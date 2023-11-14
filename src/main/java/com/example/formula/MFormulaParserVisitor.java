6340d58379d2ff393fbf95c33dc4e7580a70a196
独孤九剑 <843294669@qq.com>
2023-11-14 15:07:40 +0800

    Format code.

--- src/main/java/com/example/formula/MFormulaParserVisitor.java
+++ src/main/java/com/example/formula/MFormulaParserVisitor.java
@@ -15,87 +15,87 @@ import java.util.Map;
 
 public class MFormulaParserVisitor extends AbstractParseTreeVisitor<String> implements FormulaParserVisitor<String> {
 
-  protected Map<String, String> cellMap;
+    protected Map<String, String> cellMap;
 
-  public MFormulaParserVisitor(Map<String, String> cellMap) {
-    this.cellMap = cellMap;
-  }
-
-  @Override
-  public String visitFormula(FormulaParser.FormulaContext ctx) {
-    return "(" + accept(ctx.getChild(0)) + ")";
-  }
-
-  @Override
-  public String visitExpression(FormulaParser.ExpressionContext ctx) {
-    return accept(ctx.children);
-  }
-
-  @Override
-  public String visitCellName(FormulaParser.CellNameContext ctx) {
-    // 单元格名称为单元格的值
-    String cellValue = cellMap.get(ctx.IDENTIFIER().getText().toUpperCase());
-    if (cellValue == null) {
-      return "null";
+    public MFormulaParserVisitor(Map<String, String> cellMap) {
+        this.cellMap = cellMap;
     }
-    try {
-      return new BigDecimal(cellValue).toString();
-    } catch (Throwable e) {
-      return escapeString(cellValue);
+
+    @Override
+    public String visitFormula(FormulaParser.FormulaContext ctx) {
+        return "(" + accept(ctx.getChild(0)) + ")";
     }
-  }
 
-  @Override
-  public String visitFunctionCall(FormulaParser.FunctionCallContext ctx) {
-    return accept(ctx.children);
-  }
-
-  @Override
-  public String visitFunctionName(FormulaParser.FunctionNameContext ctx) {
-    // 替换函数名称
-    return "formulajs." + ctx.IDENTIFIER().getText().toUpperCase();
-  }
-
-  @Override
-  public String visitParameters(FormulaParser.ParametersContext ctx) {
-    return accept(ctx.children);
-  }
-
-  @Override
-  public String visitParameter(FormulaParser.ParameterContext ctx) {
-    return accept(ctx.children);
-  }
-
-  @Override
-  public String visitTerminal(TerminalNode node) {
-    return node.getText();
-  }
-
-  public String accept(ParseTree node) {
-    if (node != null) {
-      return node.accept(this);
+    @Override
+    public String visitExpression(FormulaParser.ExpressionContext ctx) {
+        return accept(ctx.children);
     }
-    return "null";
-  }
 
-  protected String accept(List<? extends ParseTree> nodes) {
-    if (nodes == null || nodes.size() == 0) {
-      return "null";
+    @Override
+    public String visitCellName(FormulaParser.CellNameContext ctx) {
+        // 单元格名称为单元格的值
+        String cellValue = cellMap.get(ctx.IDENTIFIER().getText().toUpperCase());
+        if (cellValue == null) {
+            return "null";
+        }
+        try {
+            return new BigDecimal(cellValue).toString();
+        } catch (Throwable e) {
+            return escapeString(cellValue);
+        }
     }
-    if (nodes.size() == 1) {
-      return accept(nodes.get(0));
-    }
-    StringBuilder buffer = new StringBuilder();
-    for (ParseTree node : nodes) {
-      buffer.append(accept(node));
-    }
-    return buffer.toString();
-  }
 
-  protected String escapeString(String content) {
-    SerializeWriter writer = new SerializeWriter(SerializerFeature.UseSingleQuotes);
-    writer.writeString(content);
-    return writer.toString();
-  }
+    @Override
+    public String visitFunctionCall(FormulaParser.FunctionCallContext ctx) {
+        return accept(ctx.children);
+    }
+
+    @Override
+    public String visitFunctionName(FormulaParser.FunctionNameContext ctx) {
+        // 替换函数名称
+        return "formulajs." + ctx.IDENTIFIER().getText().toUpperCase();
+    }
+
+    @Override
+    public String visitParameters(FormulaParser.ParametersContext ctx) {
+        return accept(ctx.children);
+    }
+
+    @Override
+    public String visitParameter(FormulaParser.ParameterContext ctx) {
+        return accept(ctx.children);
+    }
+
+    @Override
+    public String visitTerminal(TerminalNode node) {
+        return node.getText();
+    }
+
+    public String accept(ParseTree node) {
+        if (node != null) {
+            return node.accept(this);
+        }
+        return "null";
+    }
+
+    protected String accept(List<? extends ParseTree> nodes) {
+        if (nodes == null || nodes.size() == 0) {
+            return "null";
+        }
+        if (nodes.size() == 1) {
+            return accept(nodes.get(0));
+        }
+        StringBuilder buffer = new StringBuilder();
+        for (ParseTree node : nodes) {
+            buffer.append(accept(node));
+        }
+        return buffer.toString();
+    }
+
+    protected String escapeString(String content) {
+        SerializeWriter writer = new SerializeWriter(SerializerFeature.UseSingleQuotes);
+        writer.writeString(content);
+        return writer.toString();
+    }
 
 }

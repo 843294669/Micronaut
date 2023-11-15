cf08f4728f49c3c53aa816ba030bb8709f627484
独孤九剑 <843294669@qq.com>
2023-11-15 12:21:52 +0800

    Test JS.

--- src/test/java/com/example/MicronautDemoTest.java
+++ src/test/java/com/example/MicronautDemoTest.java
@@ -2,6 +2,7 @@ package com.example;
 
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.graalvm.polyglot.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
@@ -16,6 +17,28 @@ class MicronautDemoTest {
     @Test
     void testItWorks() {
         Assertions.assertTrue(application.isRunning());
+    }
+
+    @Test
+    void testJS() {
+        try (Context context = Context.create()) {
+            Source source = Source.create("js", "42");
+            Value value;
+            try {
+                value = context.parse(source);
+                // parsing succeeded
+            } catch (PolyglotException e) {
+                if (e.isSyntaxError()) {
+                    SourceSection location = e.getSourceLocation();
+                    // syntax error detected at location
+                } else {
+                    // other guest error detected
+                }
+                throw e;
+            }
+            // evaluate the parsed script
+            System.out.println(value.execute());
+        }
     }
 
 }
